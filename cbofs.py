382f022f680ba4a502fdafa293aa216c813bf749
Jason Greenlaw <jason.greenlaw@noaa.gov>
2018-02-25 11:17:12 -0500

    Migrated index/coefficients-related code into roms module, updated cbofs.py script to support generation of index file, and cleaned up/added comments to both s111 and roms modules.

--- cbofs.py
+++ cbofs.py
@@ -8,22 +8,25 @@ from s100ofs.s111 import S111File
 def main():
     """Parse command line arguments and execute target functions."""
     parser = argparse.ArgumentParser(description='Create or modify S-111 File')
-    parser.add_argument("s111File", help="Name of target S111 HDF5 file (should end in .h5). If the file does not already exist, it will be created.")
-    # No need to specify --create - class will automatically create file/add
-    # metadata if it doesn't already exist
-    #parser.add_argument("--create", action="store_true", help="Create/initialize a new HDF5 file")
-    parser.add_argument('-a', '--add_output', help='Path to an ocean model netcdf file to be added to the S111 HDF5.')
-    parser.add_argument('-i', '--index_file', help='Path to index and coefficient netcdf file. Required when adding model output to the S111 HDF5 file.')
+    parser.add_argument("model_output_file", help="Path to a NetCDF file containing raw model output to be converted to S111 format or used to build an index file.")
+    parser.add_argument('index_file', help='Path to index and coefficient netcdf file used when adding model output to the S111 HDF5 file, or to be created when -b is specified.')
+    parser.add_argument('-s', '--s111_file', help='Name of target S111 HDF5 file (should end in .h5). If the file does not already exist, it will be created. Ignored if -b is specified.')
+    parser.add_argument('-b', '--build_index', action='store_true', help='Build the regular grid index/coefficients NetCDF file (WARNING: Takes up to ~12 hours).')
     args = parser.parse_args()
 
-    if args.add_output and not args.index_file:
-        parser.error("--index_file (-i) required when --add_output (-a) specified.")
+    if not args.s111_file and not args.build_index:
+        parser.error("Either --s111_file or --build_index must be specified.")
         print(args)
         return 1
 
-    with S111File(args.s111File) as s111_file:
-        if args.add_output:
-            s111_file.add_output(args.add_output, args.index_file)
+    if args.build_index:
+        with roms.ROMSIndexFile(args.index_file) as index_file:
+            with roms.ROMSOutputFile(args.model_output_file) as model_output_file:
+                index_file.init_nc(model_output_file, 445, 761)
+                index_file.compute_indexes_coefficients(model_output_file)
+    elif args.s111_file:
+        with S111File(args.s111_file) as s111_file:
+            s111_file.add_output(args.model_output_file, args.index_file)
 
     return 0
 
